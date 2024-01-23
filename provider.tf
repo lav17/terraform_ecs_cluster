3f0572da81f9400c486d341dcbd3fd74bbe9fbe7
Lavisha Kapoor <kapoorlavisha0@gmail.com>
2024-01-24 00:45:30 +0530

    ecs project v2

--- provider.tf
+++ provider.tf
@@ -1,3 +1,6 @@
 provider "aws" {
-  region = "ap-south-1"
+  region     = "ap-south-1"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,12 @@
 variable "TF_VAR_CLOUDFRONT_IP" {
 
 }
+
+variable "AWS_ACCESS_KEY_ID" {
+
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+
+}
+
